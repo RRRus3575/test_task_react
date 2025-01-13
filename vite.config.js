8c30a53e8d4e90c11092e1558d52ef0f37a15cf0
Ruslan <ruslan.grudistov@gmail.com>
2025-01-13 18:03:57 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/test_task_react/", // Укажите имя вашего репозитория на GitHub
+  base: "/test_task_react/",
 });
